9de82001459e7beb2cae39624cfc71a3fbd35809
Vladislav Simovic <vladislav.simovic@acronis.com>
2024-12-13 12:59:51 +0100

    Some code reorganization

--- PseudoBrowser/ContentBlockerRequestHandler.swift
+++ PseudoBrowser/ContentBlockerRequestHandler.swift
@@ -9,17 +9,30 @@ import UIKit
 import MobileCoreServices
 
 class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+    
+    private enum ContentBlockerError: String, Error {
+        case couldNotCreateNSItemWithUrl
+    }
+    
     func beginRequest(with context: NSExtensionContext) {
         do {
             let url = try PhishingCacheFactory.shared.blockedListURL()
-            let attachment = NSItemProvider(contentsOf: url)!
-            let item = NSExtensionItem()
-            item.attachments = [attachment]
-            Logger.shared.log(message: "Loaded blocked list with attachment: \(attachment)")
+            let item = try Self.extensionItemProvider(from: url)
             context.completeRequest(returningItems: [item], completionHandler: nil)
         } catch {
             Logger.shared.log(message: error.localizedDescription, level: .error)
             context.completeRequest(returningItems: nil)
         }
+    }
+    
+    private static func extensionItemProvider(from url: URL) throws -> NSExtensionItem {
+        guard let attachment = NSItemProvider(contentsOf: url) else {
+            throw ContentBlockerError.couldNotCreateNSItemWithUrl
+        }
+        Logger.shared.log(message: "Loaded blocked list with attachment: \(attachment)")
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        return item
     }
 }
